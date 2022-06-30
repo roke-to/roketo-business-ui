8fdc07d6fbbe32d7542aec9dca2ab5811ff8e6b7
ekaterina.riazanova <rkatarine@mail.ru>
2022-06-30 14:31:34 +0300

    RB-4 AstroApi

---
+++ .scripts/generated-swagger-api-client/generator.mjs
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import swaggerTypescriptApi from 'swagger-typescript-api';
+import {argv} from 'zx';
+import 'zx/globals';
+
+const {generateApi} = swaggerTypescriptApi;
+
+const appDirectory = fs.realpathSync(process.cwd());
+
+async function main() {
+  const {resolveApp} = {
+    resolveApp: (relativePath) => path.resolve(appDirectory, relativePath),
+  };
+
+  const {
+    'api-path': apiPathRaw,
+    'output-dir': outputDirRaw,
+    'output-file': outputFileRaw = 'index.ts',
+  } = argv;
+
+  generateApi({
+    name: outputFileRaw,
+    url: apiPathRaw,
+    generateClient: true,
+    output: resolveApp(outputDirRaw),
+    generateResponses: true,
+    cleanOutput: true,
+    httpClientType: 'axios',
+  });
+}
+
+main();
