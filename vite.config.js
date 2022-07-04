c3e56fdc77d8eae961a1c5490710a21664247870
ekaterina.riazanova <rkatarine@mail.ru>
2022-07-04 13:06:33 +0300

    RB-20/RB-26 VITE_BASE_PUBLIC_PATH

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import {viteCommonjs} from '@originjs/vite-plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 import checker from 'vite-plugin-checker';
 import htmlEnv from 'vite-plugin-html-env';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import {viteCommonjs} from '@originjs/vite-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PUBLIC_PATH,
   plugins: [
     viteCommonjs(),
     react(),
