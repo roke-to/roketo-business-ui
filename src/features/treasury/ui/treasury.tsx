fe2630d42963b60e06be6f1160248cbdeece5cf6
Ekaterina Riazanova <30527539+RKatarine@users.noreply.github.com>
2022-07-11 12:26:54 +0300

    RB-45: Chips component (#21)

--- src/features/treasury/ui/treasury.tsx
+++ src/features/treasury/ui/treasury.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
 import {ProposalsList} from '~/features/treasury/ui/proposals-list';
+import {Chip} from '~/shared/ui/components/chip/Chip';
+import {Typography} from '~/shared/ui/components/typography';
 
-export const Treasury = () => <ProposalsList />;
+export const Treasury = () => (
+  <>
+    <div className='flex gap-2'>
+      <Chip>
+        <Typography as='span' weight='bold'>
+          20 NEAR
+        </Typography>
+        <Typography as='span' weight='normal' color='muted'>
+          970 USD
+        </Typography>
+      </Chip>
+      <Chip>
+        <Typography as='span' weight='bold'>
+          12wNEAR
+        </Typography>
+        <Typography as='span' weight='normal' color='muted'>
+          970 USD
+        </Typography>
+      </Chip>
+      <Chip>
+        <Typography as='span' weight='bold'>
+          120 USN
+        </Typography>
+        <Typography as='span' weight='normal' color='muted'>
+          970 USD
+        </Typography>
+      </Chip>
+    </div>
+
+    <ProposalsList />
+  </>
+);
