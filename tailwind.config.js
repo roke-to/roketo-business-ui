0b3a06d7d5a4c859d689c2546d82d8f0f95e6fe9
Ekaterina Riazanova <30527539+RKatarine@users.noreply.github.com>
2022-07-18 17:07:25 +0300

    RB-38: fix some layout bags (#34)

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
       // TODO: use color from theme
       DEFAULT: ['0px 5px 20px rgba(87, 140, 219, 0.25)'], // #578cdb
       layout: ['-5px 0px 20px rgba(87, 140, 219, 0.25)'],
+      positive: ['0px 5px 20px #d6f5df'], // colors.green.default
+      negative: ['0px 5px 20px #fccfd2'], // colors.red.default
     },
     fontFamily: {
       // TODO: нужны ли тут остальные дефолтные шрифты
