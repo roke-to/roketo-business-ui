0485f26fc3c7cd6833ee7b812073c74cc3931d16
ekaterina.riazanova <rkatarine@mail.ru>
2022-07-22 17:38:07 +0300

    RB-50: fix styles

--- src/features/treasury/ui/empty-proposal-list/empty-proposal-list.tsx
+++ src/features/treasury/ui/empty-proposal-list/empty-proposal-list.tsx
@@ -1,16 +1,31 @@
-import {useStore} from 'effector-react/effector-react.cjs';
+import {useStore} from 'effector-react';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 
 import {$treasurySelectedProposalKind, $treasurySelectedProposalStatus} from '~/entities/treasury';
+import {tailwindConfig} from '~/shared/config/tailwindConfig';
+import {useMediaQuery} from '~/shared/hook/useMediaQuery';
 import {Button} from '~/shared/ui/components/button';
 import {Modal, useModal} from '~/shared/ui/components/modal';
 import {Typography} from '~/shared/ui/components/typography';
 
 import styles from './empty-proposal-list.module.css';
 
+const TextEmptyProposalList = () => {
+  const {t} = useTranslation('treasury');
+
+  return (
+    <>
+      {t('emptyProposalList.firstLine')}
+      <br />
+      {t('emptyProposalList.secondLine')}
+    </>
+  );
+};
+
 export const EmptyProposalList = () => {
   const {t} = useTranslation('treasury');
+  const isMobileWidth = useMediaQuery(`(max-width: ${tailwindConfig.theme.screens.tablet})`);
   const createProposalModal = useModal();
 
   const treasurySelectedProposalKind = useStore($treasurySelectedProposalKind);
@@ -19,14 +34,15 @@ export const EmptyProposalList = () => {
   const isDefaultFiltersValue =
     treasurySelectedProposalKind === 'Transfer' && treasurySelectedProposalStatus === 'all';
 
-  const text = isDefaultFiltersValue ? t('emptyProposalList') : t('emptySearchProposalList');
+  const text = isDefaultFiltersValue ? <TextEmptyProposalList /> : t('emptySearchProposalList');
+  const showCreateButton = isDefaultFiltersValue || isMobileWidth;
 
   return (
     <div className={styles.container}>
-      <Typography as='span' color='muted'>
+      <Typography as='span' color='muted' align='center'>
         {text}
       </Typography>
-      {isDefaultFiltersValue && (
+      {showCreateButton && (
         <>
           <Button variant='soft' onClick={createProposalModal.show}>
             {t('list.createProposal')}
