e2a4be59ba07c597d9868d26153db918be227c78
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-07-26 14:39:47 +0300

    RB-53: add employee card component

--- src/entities/employees/ui/employee-card.tsx
+++ src/entities/employees/ui/employee-card.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+
+import {Button} from '~/shared/ui/components/button';
+import {Col} from '~/shared/ui/components/col';
+import {Label} from '~/shared/ui/components/label';
+import {Portlet} from '~/shared/ui/components/portlet';
+import {Row} from '~/shared/ui/components/row';
+import {Typography} from '~/shared/ui/components/typography';
 
 import type {Employee} from '../model/types';
 
@@ -6,6 +13,48 @@ type Props = {
   employee: Employee;
 };
 
-export const EmployeeCard: React.FC<Props> = ({employee}) => (
-  <pre>{JSON.stringify(employee, null, 2)}</pre>
-);
+export const EmployeeCard: React.FC<Props> = ({employee}) => {
+  const labelSubRenders = useMemo(() => {
+    const labeledFields = [
+      {
+        content: 'Role',
+        children: employee.role,
+      },
+      {
+        content: 'wallet address',
+        children: employee.wallet,
+      },
+      {
+        content: 'e-mail',
+        children: employee.email,
+      },
+      {
+        content: 'Salary',
+        children: `${employee.salary} (salary frequency)`,
+      },
+      {
+        content: 'Comment',
+        children: employee.comment,
+      },
+    ];
+
+    return labeledFields
+      .filter(({children}) => Boolean(children))
+      .map(({children, content}) => (
+        <Label as='div' content={content}>
+          <Typography>{children}</Typography>
+        </Label>
+      ));
+  }, [employee]);
+
+  return (
+    <Portlet>
+      <Row>
+        <Typography>{employee.name}</Typography>
+        <pre>{JSON.stringify({status: employee.status}, null, 2)}</pre>
+      </Row>
+      <Col>{labelSubRenders}</Col>
+      <Button>Actions</Button>
+    </Portlet>
+  );
+};
