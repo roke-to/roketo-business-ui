9f640eb36819d97cab636d20a53b9b64729e9b5b
extg <extg37@gmail.com>
2022-07-26 18:06:26 +0300

    RB-7: SputnikDaoContract

--- src/shared/api/sputnik-dao/contract.types.ts
+++ src/shared/api/sputnik-dao/contract.types.ts
@@ -1,7 +1,5 @@
 import {Contract} from 'near-api-js';
 
-import {Action} from '~/shared/api/sputnik-dao/consts';
-
 type ProposalInput = {
   // Description of this proposal.
   description: string;
@@ -9,8 +7,26 @@ type ProposalInput = {
   kind: any;
 };
 
+enum Action {
+  // Action to add proposal. Used internally.
+  AddProposal,
+  // Action to remove given proposal. Used for immediate deletion in special cases.
+  RemoveProposal,
+  // Vote to approve given proposal or bounty.
+  VoteApprove,
+  // Vote to reject given proposal or bounty.
+  VoteReject,
+  // Vote to remove given proposal or bounty (because it's spam).
+  VoteRemove,
+  // Finalize proposal, called when it's expired to return the funds
+  // (or in the future can be used for early proposal closure).
+  Finalize,
+  // Move a proposal to the hub to shift into another DAO.
+  MoveToHub,
+}
+
 // Sputnik DAO :: v3 see https://github.com/near-daos/sputnik-dao-contract/blob/main/sputnikdao2/ABI.md#sputnik-dao--v3
-export type SputnikDAO = Contract & {
+export type SputnikDao = Contract & {
   // viewMethods
   delegation_balance_of: any;
   delegation_balance_ratio: any;
