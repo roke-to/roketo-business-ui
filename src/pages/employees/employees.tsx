9da4808e6f12a13cee5e0e6afbdd8c0854593c5e
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-07-29 12:26:15 +0300

    RB-53: add viewType

--- src/pages/employees/employees.tsx
+++ src/pages/employees/employees.tsx
@@ -1,13 +1,16 @@
 import clsx from 'clsx';
 import {useStore} from 'effector-react';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {EmployeeCard, employeesModel} from '~/entities/employees';
 import {Button} from '~/shared/ui/components/button';
+import {Label} from '~/shared/ui/components/label';
 import {Row} from '~/shared/ui/components/row';
 import {PageLayout} from '~/widgets/page-layout';
 
 import styles from './employees.module.css';
+
+type ViewType = 'card' | 'list';
 
 export const EmployeesPage = () => {
   const employees = useStore(employeesModel.$employees);
@@ -16,15 +19,50 @@ export const EmployeesPage = () => {
     employeesModel.pageLoaded();
   }, []);
 
+  const [viewType, setViewType] = useState<ViewType>('card');
+  const handleViewTypeChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setViewType(e.target.value as ViewType);
+
   return (
     <PageLayout>
       <Row>
         <Button>Add new employee</Button>
       </Row>
-      <div className={clsx(styles.wrapper)}>
-        {employees.map((employee) => (
-          <EmployeeCard employee={employee} key={employee.id} />
-        ))}
+      <Row justify='between'>
+        <div>filters</div>
+        <Row>
+          <Label content='Card view'>
+            <input
+              type='radio'
+              name='viewType'
+              value='card'
+              checked={viewType === 'card'}
+              onChange={handleViewTypeChange}
+            />
+          </Label>
+          <Label content='List view'>
+            <input
+              type='radio'
+              name='viewType'
+              value='list'
+              checked={viewType === 'list'}
+              onChange={handleViewTypeChange}
+            />
+          </Label>
+        </Row>
+      </Row>
+      <div className={clsx(styles.wrapper, styles[`wrapper-${viewType}`])}>
+        {employees.map((employee) => {
+          if (viewType === 'card') {
+            return <EmployeeCard employee={employee} key={employee.id} />;
+          }
+
+          return (
+            <Row key={employee.id}>
+              <pre>{employee.name}</pre>
+            </Row>
+          );
+        })}
       </div>
     </PageLayout>
   );
