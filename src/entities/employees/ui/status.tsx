f54b3612afb40db811ee06e1b3c8f90f36684efc
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-07-29 15:00:50 +0300

    RB-53: fix dependency cycle

--- src/entities/employees/ui/status.tsx
+++ src/entities/employees/ui/status.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
 import React from 'react';
 
-import {Employee} from '~/entities/employees';
 import styles from '~/entities/employees/ui/status.module.css';
+
+import type {Employee} from '../model/types';
 
 type Props = React.HTMLAttributes<HTMLDivElement> & {
   status: Employee['status'];
