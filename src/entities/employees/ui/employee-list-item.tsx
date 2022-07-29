6d607e5788455e606d74d7a5e5d4b232a7ecb682
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-07-29 14:22:08 +0300

    RB-53: add styles to the EmployeeListItem component

--- src/entities/employees/ui/employee-list-item.tsx
+++ src/entities/employees/ui/employee-list-item.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 import {Status} from '~/entities/employees/ui/status';
@@ -6,18 +7,19 @@ import {Row} from '~/shared/ui/components/row';
 import {Typography} from '~/shared/ui/components/typography';
 
 import type {Employee} from '../model/types';
+import styles from './employee-list-item.module.css';
 
 type Props = React.HTMLAttributes<HTMLDivElement> & {
   employee: Employee;
 };
 
 export const EmployeeListItem: React.FC<Props> = ({employee}) => (
-  <Row justify='between' align='center'>
+  <Row justify='between' align='center' className={clsx(styles.wrapper)}>
     <Status status={employee.status} type={employee.type} />
     <Typography className='w-1/5'>{employee.name}</Typography>
     <Typography className='w-1/5'>{employee.role}</Typography>
     <Typography className='w-1/5'>{employee.wallet}</Typography>
     <Typography className='w-1/5'>{employee.salary}</Typography>
-    <Button>blah</Button>
+    <Button>...</Button>
   </Row>
 );
