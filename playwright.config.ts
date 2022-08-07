2605557d088c9399517692ec552526e180ea9d77
Anastasia <anamomotova@roke.to>
2022-08-07 20:15:49 +0300

    resolve conflicts

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,11 +36,10 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    // headless: false,
   },
 
   /* Configure projects for major browsers */
@@ -105,4 +104,5 @@ const config: PlaywrightTestConfig = {
   },
 };
 // ts-unused-exports:disable-next-line
+
 export default config;
