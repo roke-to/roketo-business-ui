9d3c2e04058e55997cf7155b2a4043dccd77268e
extg <extg@users.noreply.github.com>
2022-08-07 15:33:15 +0400

    RB-0: Exact contract options validation (#76)

--- src/shared/api/near/contracts/contract.types.ts
+++ src/shared/api/near/contracts/contract.types.ts
@@ -1,5 +1,12 @@
 import BN from 'bn.js';
 
+type Impossible<K extends keyof any> = {
+  [P in K]: never;
+};
+
+// https://stackoverflow.com/questions/49580725/is-it-possible-to-restrict-typescript-object-to-contain-only-properties-defined
+type NoExtraProperties<T, U extends T = T> = U & Impossible<Exclude<keyof U, keyof T>>;
+
 // https://github.com/near/near-api-js/blob/a05667361ddab208423f5a5c0e3dd0ce182b9880/src/contract.ts#L22
 export interface ChangeMethodOptions<T extends object> {
   args: T;
@@ -11,6 +18,8 @@ export interface ChangeMethodOptions<T extends object> {
 }
 
 export type ContractViewFunction<P, T = void> = (args?: P) => Promise<T>;
-export type ContractChangeFunction<P extends object, T = void> = (
-  args: ChangeMethodOptions<P>,
+export type ContractChangeFunction<P extends object, T = void> = <
+  Options extends ChangeMethodOptions<P>,
+>(
+  args: NoExtraProperties<ChangeMethodOptions<P>, Options>,
 ) => Promise<T>;
