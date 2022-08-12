dbd13724400255573df0cfab31be6e34579d0f01
Anastasia <anamomotova@roke.to>
2022-08-12 10:00:49 +0300

    ignore unused export

--- tests/shared/createTestAccountgenerated.ts
+++ tests/shared/createTestAccountgenerated.ts
@@ -1,6 +1,7 @@
 import {connect, keyStores, utils} from 'near-api-js';
 import {generateSeedPhrase} from 'near-seed-phrase';
 
+// remove generated from filename so it'll be checked in unused exports
 export async function createTestAccount() {
   const randomNumber = Math.floor(
     Math.random() * (99999999999999 - 10000000000000) + 10000000000000,
