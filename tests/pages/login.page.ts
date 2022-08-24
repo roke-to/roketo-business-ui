c4cab53d3f7c341e8aa93b3fcb546e8d081c4941
Anastasia <anamomotova@roke.to>
2022-08-24 13:28:22 +0300

    remove unused code

--- tests/pages/login.page.ts
+++ tests/pages/login.page.ts
@@ -5,15 +5,11 @@ export class LoginPage {
 
   readonly elements = {
     chooseDaoURL: '/dao',
-
     dashboardPageURL: '/dashboard',
-    treasuryPageURL: '/treasury',
     loginURL: '/',
     buttonSelectDao: 'button:has-text("Select DAO")',
     buttonNearWallet: 'button:has-text("NEAR Wallet")',
     accountId: '[data-qa="account"]',
-    daoSwitcher: '[data-qa="daoDropdownMenu"]',
-    choosenDao: '//button[contains(@data-qa,"daoDropdownMenu")]//div',
   };
 
   constructor(page: Page) {
@@ -40,30 +36,5 @@ export class LoginPage {
 
   async chooseNearWallet() {
     await this.page.locator(this.elements.buttonNearWallet).first().click();
-  }
-
-  async chooseCreateNewDao() {
-    await this.page.locator('//a[contains(@href,"/dao/new")]').click();
-  }
-
-  async fillNewDaoData(randomDaoName: string, randomDaoAddress: string) {
-    await this.page.locator('//input[@name="daoName"]').fill(randomDaoName);
-    // await expect(this.page.locator('input[name="daoAddress"]')).toHaveValue(${randomDaoName})
-    await this.page.locator('//input[@name="daoAddress"]').fill(randomDaoAddress);
-  }
-
-  async clickCreateDao() {
-    await this.page.locator('button', {hasText: 'Create DAO'}).click();
-  }
-
-  async clickAddCouncilsLater() {
-    await this.page.locator('button', {hasText: 'Add later'}).click();
-  }
-
-  async checkDaoExists(randomDaoAddress: string) {
-    await this.page.waitForURL(this.elements.treasuryPageURL);
-    await expect(this.page.locator(this.elements.choosenDao)).toHaveText(
-      `${randomDaoAddress}.sputnikv2.testnet`,
-    );
   }
 }
