8f42e9ff2de8754b7b61efea74b172fd5e86d416
ekaterina.riazanova <rkatarine@mail.ru>
2022-08-26 17:37:19 +0300

    RB-129: [Dao] New dao can be created only from direct link

--- src/features/dao/ui/dao-switcher.tsx
+++ src/features/dao/ui/dao-switcher.tsx
@@ -1,13 +1,20 @@
 import {useStore} from 'effector-react';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
+import {Link} from 'react-router-dom';
 
 import {$currentDaoId, $daoIds, setCurrentDaoId} from '~/entities/dao';
+import {ROUTES} from '~/shared/config/routes';
+import {Button} from '~/shared/ui/components/button';
 import {DropdownMenu} from '~/shared/ui/components/dropdown-menu';
 import {DropdownContent} from '~/shared/ui/components/dropdown-menu/dropdown-content';
 import {DropdownItem} from '~/shared/ui/components/dropdown-menu/dropdown-item';
 import {RadioGroupItem} from '~/shared/ui/components/radio-group';
 
+import styles from './dao-switcher.module.css';
+
 export const DaoSwitcher = ({className}: {className?: string}) => {
+  const {t} = useTranslation('dao');
   const daoId = useStore($currentDaoId);
   const userDaos = useStore($daoIds);
 
@@ -23,6 +30,11 @@ export const DaoSwitcher = ({className}: {className?: string}) => {
             <RadioGroupItem value={id} label={id} checked={id === daoId} />
           </DropdownItem>
         ))}
+
+        {/* @ts-expect-error */}
+        <Button as={Link} to={ROUTES.daoNew.path} variant='soft' className={styles.createDao}>
+          {t('daoInit.createDao')}
+        </Button>
       </DropdownContent>
     </DropdownMenu>
   );
