bb6a98c05d2dcb72fc23ba70ef48fbba2984b82d
extg <extg@users.noreply.github.com>
2022-09-06 20:16:11 +0400

    RB-176: Add streams to dashboard (#122)

---
+++ src/pages/streams.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {StreamsPageContent} from '~/entities/streams';
+import {Layout} from '~/shared/ui/components/layout';
+
+export const StreamsPage = () => (
+  <Layout>
+    <StreamsPageContent />
+  </Layout>
+);
