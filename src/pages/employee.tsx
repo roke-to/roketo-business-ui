d368db5dc050a1093ca1d86519c5558bed47fdbd
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-09-06 19:35:55 +0300

    RB-57: add useParams to the employee.tsx

--- src/pages/employee.tsx
+++ src/pages/employee.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 
 import {Layout} from '~/shared/ui/components/layout';
 
-export const EmployeePage = () => <Layout>Employee Page</Layout>;
+export const EmployeePage = () => {
+  const {id} = useParams<{id: string}>();
+
+  return <Layout>Employee Page #{id}</Layout>;
+};
