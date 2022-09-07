58998072852cb8b65b6f4ccfd09be1fa884c1928
Anastasia <anamomotova@roke.to>
2022-09-07 12:18:45 +0300

    change user

--- tests/fixtures/near-authenticated.ts
+++ tests/fixtures/near-authenticated.ts
@@ -5,7 +5,7 @@ import {test as base} from '@playwright/test';
 export const test = base.extend({
   // eslint-disable-next-line no-empty-pattern
   accountId: async ({}, use) => {
-    use('rocketotestuser.testnet');
+    use('rocketobiztestuser.testnet');
   },
   page: async ({browser}, use) => {
     const context = await browser.newContext({
