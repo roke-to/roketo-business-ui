0e2e8191150f8371cac4dd049c85f558bc6a1e1b
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-09-08 00:59:24 +0300

    RB-57: add styles for the employee name

--- src/entities/employee/ui/employee.tsx
+++ src/entities/employee/ui/employee.tsx
@@ -1,9 +1,39 @@
 import {useStore} from 'effector-react';
 import React from 'react';
 
+import {Col} from '~/shared/ui/components/col';
+import {Row} from '~/shared/ui/components/row';
+import {Typography} from '~/shared/ui/components/typography';
+
 import * as employeeModel from '../model/employee-model';
 
 export const Employee: React.FC = () => {
   const employee = useStore(employeeModel.$employee);
-  return <pre>{JSON.stringify({employee}, null, 2)}</pre>;
+  return (
+    <>
+      <Row>
+        <Col className='basis-3/4'>
+          <Typography as='h2' font='heading'>
+            {employee?.name}
+          </Typography>
+          <span>type</span>
+          <span>position</span>
+          <span>wallet address</span>
+          <span>e-mail</span>
+          <span>salary</span>
+          <span>Invoice period</span>
+          <span>Payout type</span>
+          <span>Comment</span>
+          <div>proposal</div>
+          <div>actions history</div>
+        </Col>
+        <Row>
+          <span>...</span>
+          <span>Create stream</span>
+        </Row>
+      </Row>
+
+      <pre>{JSON.stringify({employee}, null, 2)}</pre>
+    </>
+  );
 };
