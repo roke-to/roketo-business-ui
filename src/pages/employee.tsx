a9f6d236ee807a7c88e9d95aa6d5a5847815817c
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-09-07 10:52:41 +0300

    RB-57: add pageLoaded event to the useEffect in employee.tsx

--- src/pages/employee.tsx
+++ src/pages/employee.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
+import {employeeModel} from '~/entities/employee';
 import {Layout} from '~/shared/ui/components/layout';
 
 export const EmployeePage = () => {
   const {id} = useParams<{id: string}>();
+
+  useEffect(() => {
+    employeeModel.pageLoaded(id);
+  }, [id]);
 
   return <Layout>Employee Page #{id}</Layout>;
 };
