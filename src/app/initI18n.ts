d039966c826178f81ea33223b0f087c375c9f9cc
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-09-08 13:23:38 +0300

    RB-57: add styles for employee fields

--- src/app/initI18n.ts
+++ src/app/initI18n.ts
@@ -3,6 +3,7 @@ import {initReactI18next} from 'react-i18next';
 
 import councilsEn from '../entities/councils/lib/i18n/councilsEn.json';
 import dashboardEn from '../entities/dashboard/lib/i18n/dashboardEn.json';
+import employeeEn from '../entities/employee/i18n/employeeEn.json';
 import employeesEn from '../entities/employees/i18n/employeesEn.json';
 import proposalFiltersEn from '../entities/filters/lib/i18n/proposalFiltersEn.json';
 import proposalEn from '../entities/proposal/lib/i18n/proposalEn.json';
@@ -23,6 +24,7 @@ const resources: Record<Locale, ResourceLanguage> = {
     councils: councilsEn,
     dashboard: dashboardEn,
     employees: employeesEn,
+    employee: employeeEn,
     proposalFilters: proposalFiltersEn,
   },
 };
@@ -39,6 +41,7 @@ declare module 'react-i18next' {
       councils: typeof councilsEn;
       dashboard: typeof dashboardEn;
       employees: typeof employeesEn;
+      employee: typeof employeeEn;
       proposalFilters: typeof proposalFiltersEn;
     };
   }
--- src/entities/employee/ui/employee.tsx
+++ src/entities/employee/ui/employee.tsx
@@ -1,7 +1,9 @@
 import {useStore} from 'effector-react';
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 
 import {Col} from '~/shared/ui/components/col';
+import {Label} from '~/shared/ui/components/label';
 import {Row} from '~/shared/ui/components/row';
 import {Typography} from '~/shared/ui/components/typography';
 
@@ -9,36 +11,52 @@ import * as employeeModel from '../model/employee-model';
 import {EmployeeType} from './employee-type';
 
 export const Employee: React.FC = () => {
+  const {t} = useTranslation('employee');
   const employee = useStore(employeeModel.$employee);
   return (
-    <>
+    employee && (
       <Row>
         <Col className='basis-3/4'>
           <Row align='center' gap='sm'>
             <Typography as='h2' font='heading'>
-              {employee?.name}
+              {employee.name}
             </Typography>
-            {employee && <EmployeeType type={employee.type} status={employee.status} />}
+            <EmployeeType type={employee.type} status={employee.status} />
           </Row>
-          <div className='grid gap-4 desktop:grid-cols-3'>
-            <span>position</span>
-            <span>wallet address</span>
-            <span>e-mail</span>
-            <span>salary</span>
-            <span>Invoice period</span>
-            <span>Payout type</span>
-            <span className='desktop:col-span-3'>Comment</span>
+          <div className='grid gap-6 mb-4 desktop:grid-cols-3'>
+            <Label as='div' content={t('labels.position')}>
+              <Typography>{employee.position}</Typography>
+            </Label>
+            <Label as='div' content={t('labels.nearLogin')}>
+              <Typography>{employee.nearLogin}</Typography>
+            </Label>
+            <Label as='div' content={t('labels.email')}>
+              <Typography>{employee.email || '—'}</Typography>
+            </Label>
+            <Label as='div' content={t('labels.salary')}>
+              <Typography>{employee.salary}</Typography>
+            </Label>
+            <Label as='div' content={t('labels.payPeriod')}>
+              <Typography>{employee.payPeriod}</Typography>
+            </Label>
+            <Label as='div' content={t('labels.payoutType')}>
+              {/* TODO у нас нет под это поля в базе */}
+              <Typography>Smooth</Typography>
+            </Label>
+            {employee.comment && (
+              <Label as='div' content={t('labels.comment')} className='desktop:col-span-3'>
+                <Typography>{employee.comment}</Typography>
+              </Label>
+            )}
           </div>
           <div>proposal</div>
           <div>actions history</div>
         </Col>
         <Row>
-          <span>...</span>
           <span>Create stream</span>
+          <span>...</span>
         </Row>
       </Row>
-
-      <pre>{JSON.stringify({employee}, null, 2)}</pre>
-    </>
+    )
   );
 };
