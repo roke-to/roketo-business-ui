1495da9af7dfc5e07408e0b8dcaca97232516ab3
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2022-10-06 10:43:42 +0100

    RB-204: update text in DraftInvoice

--- src/entities/employees/ui/draft-invoice.tsx
+++ src/entities/employees/ui/draft-invoice.tsx
@@ -8,13 +8,13 @@ export const DraftInvoice: React.FC<{
   clickHandler: () => void;
   draftInvoice: employeesModel.TemporaryStubForDraftInvoicesDTO;
 }> = ({clickHandler, draftInvoice}) => {
-  const {amount, token} = draftInvoice;
+  const {amount, token, periodStart, periodEnd} = draftInvoice;
 
   return (
     <div className='flex justify-between px-8 pt-6 pb-8 gap-8 border border-blue-sat_1 rounded-3xl'>
       <span>
         Appoint a salary by transfer proposal of {amount} {token} to ${'{employee_name}'} for the
-        remaining billing period (${'{remaining_billing_period}'})
+        remaining billing period (from {periodStart} to {periodEnd})
       </span>
       <CreateTreasuryProposalButton onClick={clickHandler}>Propose</CreateTreasuryProposalButton>
     </div>
