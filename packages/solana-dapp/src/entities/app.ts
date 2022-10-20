078ff99d146884dae5c1c1a39b7aeeb0e69f8931
ekaterina.riazanova <rkatarine@mail.ru>
2022-10-20 14:17:11 +0300

    Rb-253: [Infra] Template gh actions

--- packages/solana-dapp/src/entities/app.ts
+++ packages/solana-dapp/src/entities/app.ts
@@ -1,9 +1,15 @@
-import {createEvent, sample} from "effector";
-import {initWallet} from "./wallet";
+import {createEvent, sample} from 'effector';
+
+import {env} from '~/shared/config/env';
+
+import {initWallet} from './wallet';
 
 export const initApp = createEvent();
 
 sample({
   clock: initApp,
+  fn: () => {
+    console.log('env', env);
+  },
   target: initWallet,
 });
